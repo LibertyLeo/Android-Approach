3390eee17d5e0d405af5e8a9e88fab026e873477
Lei Zijian <libertyleo@163.com>
2017-09-11 15:48:45 +0800

    Some files was left for the last push.

---
+++ Demos/PhotoGallery/Looper_HandlerAndHandlerThread/app/src/main/java/com/libertyleo/photogallery/ThumbnailDownloader.java
@@ -0,0 +1,105 @@
+package com.libertyleo.photogallery;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by Leo_Lei on 9/11/17.
+ */
+
+public class ThumbnailDownloader<T> extends HandlerThread {
+    private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 0;
+
+    private boolean mHasQuit = false;
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequsetMap = new ConcurrentHashMap<>();
+    private Handler mResponseHandler;
+    private ThumbnailDownloaderListener<T> mThumbnailDownloaderListener;
+
+    public interface ThumbnailDownloaderListener<T> {
+        void onThumbnailDownloaded(T target, Bitmap thumbnail);
+    }
+
+    public void setThumbnailDownloaderListener(ThumbnailDownloaderListener<T> listener) {
+        mThumbnailDownloaderListener = listener;
+    }
+
+    public ThumbnailDownloader(Handler responseHandler) {
+        super(TAG);
+        mRequestHandler = responseHandler;
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "Got a request for URL: " + mRequsetMap.get(target));
+                    handleRequest(target);
+                }
+            }
+        };
+    }
+
+    @Override
+    public boolean quit() {
+        mHasQuit = true;
+        return super.quit();
+    }
+
+    public void queueThumbnail(T target, String url) {
+        Log.i(TAG, "Got a URL: " + url);
+
+        if (url == null) {
+            mRequsetMap.remove(target);
+        } else {
+            mRequsetMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+                    .sendToTarget();
+        }
+    }
+
+    public void clearQueue() {
+        mRequestHandler.removeMessages(MESSAGE_DOWNLOAD);
+        mRequsetMap.clear();
+    }
+
+    private void handleRequest(final T target) {
+        try {
+            final String url = mRequsetMap.get(target);
+
+            if (url == null) {
+                return;
+            }
+
+            byte[] bitmapBytes = new FlickrFetcher().getUrlBytes(url);
+            final Bitmap bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.length);
+            Log.i(TAG, "Bitmap created");
+
+            mResponseHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (mRequsetMap.get(target) != url || mHasQuit) {
+                        return;
+                    }
+
+                    mRequsetMap.remove(target);
+                    mThumbnailDownloaderListener.onThumbnailDownloaded(target, bitmap);
+                }
+            });
+        } catch (IOException e) {
+            Log.e(TAG, "Error downloading image", e);
+        }
+    }
+}
